b0e94d8586e8837370ba47b4685d30a4631db0e6
Gerald Calvo <geraldcalvoah@gmail.com>
2019-04-23 21:22:03 -0600

    Se crea la clase Token

--- Token.cpp
+++ Token.cpp
@@ -1,1 +1,86 @@
 #include "Token.h"
+
+Token::Token(Tipo k)
+{
+	tipo = k;
+}
+
+Tipo Token::demeTipo()
+{
+	return tipo;
+}
+
+
+Token::Token(string st)
+{
+	tipo = nulo;
+	tokenS = st;
+}
+
+Token::Token(string st, Tipo k)
+{
+	tipo = k;
+	tokenS = st;
+}
+
+Token::Token(int st, Tipo k)
+{
+	tipo = k;
+	num = st;
+}
+
+Token::Token(char s, Tipo k)
+{
+	tipo = k;
+	tokenC = s;
+}
+
+Token::Token(int n)
+{
+	tipo = numero;
+	num = n;
+}
+
+Token::Token() {
+	tipo = nulo;
+}
+
+ostream& operator <<(ostream &os, Token & t) {
+	switch (t.tipo) {
+	case variable: {
+		os << "[" << t.tokenS << "]" << "  variable\n";
+		break;
+	}
+	case operador:
+		os << "[" << t.tokenC << "]" << "   operador\n";
+		break;
+	case numero:
+		os << "[" << t.num << "]" << "   numero\n";
+		break;
+	case siguiente:
+		os << "[" << t.tokenS << "]" << " siguiente\n";
+		break;
+	case aleatorio:
+		os << "[" << t.tokenC << "]" << " aleatorio\n";
+		break;
+	case nuevo:
+		os << "[" << t.tokenS << "]" << " nuevo\n";
+		break;
+	case parentesis:
+		os << "[" << t.tokenC << "]" << " parentesis\n";
+		break;
+	case repetir:
+		os << "[" << t.tokenS << "]" << " repetir\n";
+		break;
+	case mientras:
+		os << "[" << t.tokenS << "]" << " mientras\n";
+		break;
+	case heapback:
+		os << "[" << t.tokenS << "]" << " heapback\n";
+		break;
+	case nulo:
+		os << "[ ]" << "  nulo\n";
+		break;
+	}
+	return os;
+}
--- Token.h
+++ Token.h
@@ -3,3 +3,24 @@
 
 using namespace std;
 
+enum Tipo {
+	variable, operador, numero, aleatorio,
+	nulo, siguiente, nuevo, parentesis, repetir, heapback, mientras
+};
+struct Token {
+	Tipo tipo;
+	string tokenS;
+	int num;
+	char tokenC;
+
+	Token(string st);
+	Token(string st, Tipo k);
+	Token(int st, Tipo k);
+	Token(char s, Tipo k);
+	Token(int n);
+	Token();
+	Token(Tipo k);
+	Tipo demeTipo();
+	friend ostream& operator<<(ostream &os, Token & t);
+	~Token() {};
+};
