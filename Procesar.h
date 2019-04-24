756590f2af9b1771c0d505041a187076cfb531a3
Gerald Calvo <geraldcalvoah@gmail.com>
2019-04-23 21:27:48 -0600

    Se crea el Procesar.h

--- Procesar.h
+++ Procesar.h
@@ -9,15 +9,36 @@
 #include "Token.h"
 using namespace std;
 
+struct letra {
+	string variable;
+	enlace p;
+	letra() :variable{ "" }, p{ nullptr }{};
+	letra(string p, enlace q) : variable{ p }, p{ q }{};
+	letra(string p) : variable{ p }, p{ NULL }{};
+};
 
-class Procesar:public Tokenizador{
+class Procesar :public Tokenizador {
 private:
 	In_box * in;
+	Simple_window * win;
 	Fl_Button *boton1;
 	Fl_Button *boton2;
 	Fl_Button *boton3;
 	Fl_Button *boton4;
+	letra variables[26];
+	vector<vector<Token>> instrucciones;
+	vector<string> strInstrucciones;
 	Fl_Output *salida;
+	vector<Celda *>celdas;
+	vector<Text *>labels;
+	vector<Text*>referencias;
+	vector<Arco *> arcos;
+	void cargarHeap();
+	vector<Token>tokensAux;
+	enlace demeNodo(string);
+	void asignaNodo(enlace, string);
+	int averiguaTipo(int, int);
+
 	
 
 public:
